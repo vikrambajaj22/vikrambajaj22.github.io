ba895f5a55ecb76e9daac01f399cf7b28a12648e
Vikram Bajaj <vikrambajaj@nyu.edu>
2025-03-06 20:09:11 -0500

    add AI Sight files

--- quartz.config.ts
+++ quartz.config.ts
@@ -67,7 +67,9 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
+      Plugin.TableOfContents({
+        "maxDepth": 6
+      }),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
