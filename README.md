5b9b47070323587f557a9e12c85394c113e6ba8b
Vikram Bajaj <vikrambajaj@nyu.edu>
2025-03-08 00:34:09 -0500

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # vikrambajaj22.github.io
 
 Personal Website built using [Obsidian](http://obsidian.md) and [Quartz 4.0](https://quartz.jzhao.xyz).
+
+Run locally using: `npx quartz build --serve`
--- quartz/components/Search.tsx
+++ quartz/components/Search.tsx
@@ -22,7 +22,7 @@ export default ((userOpts?: Partial<SearchOptions>) => {
         <button class="search-button" id="search-button">
           {/* <p>{i18n(cfg.locale).components.search.title}</p> */}
           <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7">
-            <title>Search</title>
+            {/* <title>Search</title> */}
             <g class="search-path" fill="none">
               <path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4" />
               <circle cx="8" cy="8" r="7" />
