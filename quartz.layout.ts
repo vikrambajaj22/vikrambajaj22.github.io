8181e9533b86878191f6f80beb258c3c65f167a1
Vikram Bajaj <vikrambajaj@nyu.edu>
2025-03-11 22:04:44 -0400

    enable reading time (content meta)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,7 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    // Component.ContentMeta(),
+    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
